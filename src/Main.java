d7a6b5bd53a5bfb5fe56882a291deee09fd4def7
Thilina <thilina@arimac.ws>
2014-08-20 17:01:06 +0530

    NER 1

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,43 @@
-import org.jsoup.*;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.select.Elements;
 
-import java.io.IOException;
+import NER.NER_Extractor;
+import PDF.PDF_Reader;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Google g=new Google();
-        LinkedInExtractor linkedIn=new LinkedInExtractor();
-        String link=g.FindOnLinkedIn("malaka walpola");             // insert name here
-        System.out.println(link);
-        linkedIn.Extract(link);
+//        Pattern findUrl = Pattern.compile("\\bhttp.*?\\.pdf\\b");
+//        Matcher matcher = findUrl.matcher("\n" +
+//                " Organizer: <PERSON>Dayata Kirula</PERSON> 2011 <ORGANIZATION>CSE</ORGANIZATION>’s <ORGANIZATION>Exhibition Stall</ORGANIZATION>. ");
+//        while (matcher.find()) {
+//            System.out.println(matcher.group());
+//        }
+
+//        String input = "one Organizer: <PERSON>Dayata Kirula</PERSON> 2011 <ORGANIZATION>CSE</ORGANIZATION>’s <ORGANIZATION>Exhibition Stall</ORGANIZATION>.  two;";
+//        Pattern p = Pattern.compile("(?<=\\bPERSON>\\b).*?(?=\\b</PERSON\\b)");
+//        Matcher m = p.matcher(input);
+//        List<String> matches = new ArrayList<String>();
+//        while (m.find()) {
+//            matches.add(m.group());
+//        }
+//        System.out.println(matches.toString());
+//        PDF_Reader rd=new PDF_Reader();
+//        rd.SaveTxt("Res/CV.pdf","Res/CV.txt");
+//
+        NER_Extractor ner=new NER_Extractor();
+        ner.ExtractNames();
+        //============== extracting linkedIn profile info
+
+//        Google g=new Google();
+//        LinkedInExtractor linkedIn=new LinkedInExtractor();
+//        String link=g.FindOnLinkedIn("malaka walpola");             // insert name here
+//        System.out.println(link);
+//        linkedIn.Extract(link);
     }
 }
