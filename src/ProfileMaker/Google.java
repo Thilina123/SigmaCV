68ac9473f734caf43cfc7c9a0fdb8fe218d9e87a
thilina <teez.mrt@gmail.com>
2014-08-28 18:36:09 +0530

    Google Scholar

--- src/ProfileMaker/Google.java
+++ src/ProfileMaker/Google.java
@@ -4,29 +4,60 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import utils.NetworkManager;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 import java.io.IOException;
 
-/**
- * Created by MALMI on 7/26/2014.
- */
 public class Google {
     public String FindOnLinkedIn(String name){
         String link="";
         Document doc = null;
         try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxyHost", 1234));
             doc = Jsoup.connect("https://www.google.lk/search?q="+name.replace(' ','+')+"+linkedin").userAgent("Chrome").timeout(0).get();
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         Elements elements;
-//        elements = doc.select("a[href*=linkedin.com]>div");
         elements = doc.select("cite");
 
-//        for (int i = 0; i < elements.size(); i++) {
-//            System.out.println(elements.get(i).text());
-//        }
-        link="https://"+elements.get(0).text();
+        int i=0;
+        while(elements.size()>i) {
+            link = "https://" + elements.get(i).text();
+            i++;
+            if (!link.contains("/dir/"))
+                break;
+        }
+        System.out.println(link);
+        return  link;
+    }
+    public String FindOnGoogleScholar(String name){
+        String link="";
+        Document doc = null;
+        try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxyHost", 1234));
+            doc = Jsoup.connect("https://www.google.lk/search?q="+name.replace(' ','+')+"+google+scholar").userAgent("Chrome").timeout(0).get();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements elements;
+        elements = doc.select("cite");
+
+        int i=0;
+        while(elements.size()>i) {
+            link = "https://" + elements.get(i).text();
+            i++;
+            if (link.contains(".com/citations?user="))
+                break;
+        }
+        System.out.println(link);
         return  link;
     }
 }
